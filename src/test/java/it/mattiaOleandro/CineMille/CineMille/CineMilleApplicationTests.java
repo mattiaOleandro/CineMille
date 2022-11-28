7c4d6d254525eff34fd3a377ccefa5a6def15db8
Mattia <oleandromattia@gmail.com>
2022-11-28 18:55:05 +0100

    Initial commit

---
+++ src/test/java/it/mattiaOleandro/CineMille/CineMille/CineMilleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mattiaOleandro.CineMille.CineMille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineMilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
